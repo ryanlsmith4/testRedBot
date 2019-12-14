102ff853ce54f3f0de31373fe6c0ce5c6b136195
Ryan <ryan.l.smith4@gmail.com>
2019-12-13 17:03:29 -0800

    working but not keeping track of times called

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+'use strict';
+
 require('dotenv').config();
 
 const BOT_START = Date.now() / 1000;
 
+const COUNT = 0;
+
 const Snoowrap = require('snoowrap');
 
 const { CommentStream } = require('snoostorm');
+
+const reply = (comment) => {
+  const lowCom =  comment.toLowerCase();
+  if(lowCom.includes('prison mike')) {
+    COUNT + 1;
+    return true;
+  };
+  return false;
+};
 
 const client = new Snoowrap({
   userAgent: 'testBot: v1.0.0 (by /u/Dry_Relation)',
@@ -15,13 +28,22 @@ const client = new Snoowrap({
 });
 
 const comments = new CommentStream(client, {
-  subreddit: 'testingground4bots',
-  limit: 10,
-  pollTime: 10000,
+  subreddit:'all',
+  pollTime: 3000,
+  limit: 10000,
+});
+
+comments.on('error', (e) => {
+  console.log('Something went wrong');
+  console.log(e);
 });
 
 comments.on('item', (item) => {
-  console.log('HERE');
+  const text = `https://giphy.com/gifs/aZeFIjI9hNcJ2/html5 I am a bot BleepBoop this bot has been called ${COUNT}`
+
+  console.log('listening for comments');
   if(item.created_utc < Math.floor(BOT_START)) return; 
-  console.log(item);
-});
+  if(reply(item.body)){
+    item.reply(text);
+  };
+});3
