758584ea5e49d351fd22951ccaae48dbad93569a
Ryan <ryan.l.smith4@gmail.com>
2019-12-15 16:38:06 -0800

    getting replies length from reddit

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ require('dotenv').config();
 
 const BOT_START = Date.now() / 1000;
 
-let COUNT = 1;
-
 const Snoowrap = require('snoowrap');
 
 const { CommentStream } = require('snoostorm');
-
+// let COUNT = 0;
 const reply = (comment) => {
   const lowCom =  comment.toLowerCase();
   if(lowCom.includes('prison mike')) {
@@ -29,7 +27,7 @@ const client = new Snoowrap({
 
 const comments = new CommentStream(client, {
   subreddit:'all',
-  pollTime: 1000,
+  pollTime: 3000,
   limit: 1000,
 });
 
@@ -37,15 +35,22 @@ comments.on('error', (e) => {
   console.log('Something went wrong');
   console.log(e);
 });
+
 let n = 0;
-comments.on('item', (item) => {
-  const text = `https://giphy.com/gifs/aZeFIjI9hNcJ2/html5 I am a bot BleepBoop ${COUNT} `;
-  // console.log(item.body)
+comments.on('item', async (item) => {
+  const user = client.getUser(process.env.USERNAME);
+  let dryComments =  await user.getComments();
+
+  let COUNT = dryComments.length
+
+  console.log(COUNT);
+  // https://giphy.com/gifs/aZeFIjI9hNcJ2/html5
+  const text = `https://media.giphy.com/media/aZeFIjI9hNcJ2/giphy.gif  <br /> I am a bot BleepBoop this bot has been summoned ${COUNT} times`;
   if(item.created_utc < Math.floor(BOT_START)) return; 
   n += 1
   console.log('listening for comments ' + n);
   if(reply(item.body)){
-    console.log("count " + COUNT)
+    console.log("count " + COUNT);
     item.reply(text);
   };
 });
