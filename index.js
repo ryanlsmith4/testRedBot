2fa8aa35ab06a9b1d64d5e24faede3e12c115483
Ryan <ryan.l.smith4@gmail.com>
2019-12-20 22:47:41 -0800

    working on dev branch

--- index.js
+++ index.js
@@ -3,29 +3,31 @@
 
 require('dotenv').config();
 
-const BOT_START = Date.now() / 1000;
+// const BOT_START = Date.now() / 1000;
 
-const fs = require('fs');
+// const fs = require('fs');
 
 const Snoowrap =  require('snoowrap');
 
 const { CommentStream } = require('snoostorm');
 
+const bots = [];
+
 const reply = (comment) => {
 	const lowCom =  comment.toLowerCase();
-	if(lowCom.includes('prison mike')) {
-		console.log('Bot summoned...');
+	if(lowCom.includes('bot')) {
+    commentsRn += 1;
 		return true;
 	}
 	return false;
 };
 
-const save = async(count, fs) => {
-	await fs.writeFile('count.txt', count, (err) => {
-		if(err) throw err;
-		console.log('Saved Count ', count);
-	});
-};
+// const save = async(count, fs) => {
+// 	await fs.writeFile('count.txt', count, (err) => {
+// 		if(err) throw err;
+// 		console.log('Saved Count ', count);
+// 	});
+// };
 
 const client = new Snoowrap({
 	userAgent: 'testBot: v1.0.0 (by /u/Dry_Relation)',
@@ -39,7 +41,7 @@ client.config({ continueAfterRatelimitError: true });
 const comments = new CommentStream(client, {
 	subreddit:'all',
 	pollTime: 1000,
-	limit: 100,
+	// limit: 100,
 });
 
 comments.on('error', (e) => {
@@ -47,26 +49,38 @@ comments.on('error', (e) => {
 	console.log(e);
 });
 
-const fileCount = fs.readFileSync('count.txt', 'utf8');
+// const fileCount = fs.readFileSync('count.txt', 'utf8');
 
-let count = Number(fileCount);
+// let count = Number(fileCount);
 console.log('Listening for comments');
+let commentsRn = 0;
 comments.on('item', async (item) => {
-	// Avoid hitting rate limit by setting timeout.
-	await new Promise(r => {
-		setTimeout(r, 1000);
-	});
-  
-	if(item.created_utc < BOT_START) return;
-  
-	if(reply(item.body)){
-		count += 1;
-		await save(count, fs);
-		let text = `https://media.giphy.com/media/aZeFIjI9hNcJ2/giphy.gif &nbsp;
-    
-    I am a bot BleepBoop This bot has been summoned ${count} times 
-
-   [link to GitHub](https://github.com/ryanlsmith4/testRedBot)`;
-		await item.reply(text);
+	if(item){
+    // commentsRn += 1;
+			console.log('Bot summoned...');
+		// console.log(item.body, commentsRn);
+		if(reply(item.body)){
+      bots.push(item.body, commentsRn);
+      console.log(bots)
+		}
 	}
+ 
+  
+	// Avoid hitting rate limit by setting timeout.
+	// await new Promise(r => {
+	// 	setTimeout(r, 1000);
+	// });
+  
+	// if(item.created_utc < BOT_START) return;
+  
+	// if(reply(item.body)){
+	// 	count += 1;
+	// 	await save(count, fs);
+	// 	let text = `https://media.giphy.com/media/aZeFIjI9hNcJ2/giphy.gif &nbsp;
+    
+	//   I am a bot BleepBoop This bot has been summoned ${count} times 
+
+	//  [link to GitHub](https://github.com/ryanlsmith4/testRedBot)`;
+	// 	await item.reply(text);
+
 });
